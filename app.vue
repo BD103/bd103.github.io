eefed4d15c6e358326a369f0e223a2f778185da4
BD103 <59022059+BD103@users.noreply.github.com>
2023-07-12 18:45:46 -0400

    feat: inline sidebar component

--- app.vue
+++ app.vue
@@ -1,7 +1,39 @@
 <template>
     <NuxtLayout>
         <template #sidebar>
-            <Sidebar />
+            <h1>BD103</h1>
+
+            <ul>
+                <li>
+                    <NuxtLink to="/">
+                        Home
+                    </NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/blog">
+                        Blog
+                    </NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="https://github.com/BD103" target="_blank">
+                        Github
+                    </NuxtLink>
+                </li>
+            </ul>
+
+            <p>
+                <NuxtLink to="https://github.com/BD103/bd103.github.io" target="_blank">
+                    Source Code
+                </NuxtLink>
+            </p>
+            <p>
+                <NuxtLink to="/feed.xml" target="_blank">
+                    RSS
+                </NuxtLink>,
+                <NuxtLink to="/atom.xml" target="_blank">
+                    Atom
+                </NuxtLink>
+            </p>
         </template>
 
         <NuxtPage />
