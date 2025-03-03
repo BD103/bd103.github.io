68178689cbd39e33d5adec3ef9400615c32afe71
BD103 <59022059+BD103@users.noreply.github.com>
2025-03-03 11:41:31 -0500

    feat: switch to static rendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from "mdsvex";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,10 +9,9 @@ const config = {
   preprocess: [vitePreprocess(), mdsvex()],
 
   kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: "404.html",
+    }),
   },
 
   extensions: [".svelte", ".svx"],
