efc0ce58cd3736240fb2e50ef3b50e2ccf37aa8a
BD103 <59022059+BD103@users.noreply.github.com>
2025-03-03 11:07:02 -0500

    chore!: delete nuxt website

--- composables/use-blog.ts
+++
@@ -1,4 +0,0 @@
-export const useBlog = () => ({
-    // Returns a list of all blogs posts (title and _path)
-    nav: async () => (await fetchContentNavigation(queryContent("blog")))[0].children,
-});
