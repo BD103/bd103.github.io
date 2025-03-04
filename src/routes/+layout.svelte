50cedc578340ef5dad769fadbf388ca5ed6b582f
BD103 <59022059+BD103@users.noreply.github.com>
2025-03-04 10:30:46 -0500

    feat: setup layout with mobile responsiveness

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  let { children } = $props();
+</script>
+
+<div class="grid">
+  <nav>
+    <h1>BD103</h1>
+
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li>Blog</li>
+      <li>History</li>
+      <li>Github</li>
+      <li>Source Code</li>
+      <li>RSS, Atom</li>
+    </ul>
+  </nav>
+  <main>{@render children()}</main>
+</div>
+
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+  }
+
+  /* When on mobile, switch to a single-column grid. */
+  @media (width <= 500px) {
+    .grid {
+      grid-template-columns: 1fr;
+    }
+  }
+</style>
