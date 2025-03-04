cd1588200bb1c7a17754d0fa0f4c420b6b9e53c1
BD103 <59022059+BD103@users.noreply.github.com>
2025-03-04 10:57:20 -0500

    feat: switch `layout.js` to typescript and populate home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Hello!</h1>
+
+<p>Welcome to my website / blog!</p>
