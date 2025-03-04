a283ee7697148ac1bdb32d1d4020e19c9f79c0df
BD103 <59022059+BD103@users.noreply.github.com>
2025-03-04 18:11:32 -0500

    refactor: make the homepage a `.svelte` file again

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  let { data } = $props();
+</script>
+
+<h1>Hello!</h1>
+
+<p>
+  Welcome to my website and blog! I'm BD103, and I'm an open-sourced contributor with a strong
+  interest in game development, tooling, and infrastructure.
+</p>
+
+<p>
+  I've been an active contributor and community member of <a href="https://bevyengine.org">Bevy</a>,
+  a game engine written in <a href="https://www.rust-lang.org">Rust</a>, since November 2023. You'll
+  find many of my projects and blog posts are related to Bevy and Rust in general!
+</p>
+
+<h2>Posts</h2>
+
+<ul>
+  {#each data.posts as post}
+    <li><a href={post.href}>{post.title}</a></li>
+  {/each}
+</ul>
