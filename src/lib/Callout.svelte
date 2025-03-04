b87fc5d28407f26d437d448d9ffcfc165a1a0c3c
BD103 <59022059+BD103@users.noreply.github.com>
2025-03-04 16:42:13 -0500

    feat: create callout component

---
+++ src/lib/Callout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  interface Props {
+    kind: "note" | "tip" | "important" | "warning" | "caution",
+    body: Snippet,
+  }
+
+  const { kind, body }: Props = $props();
+
+  // Capitalize the first letter of `kind` to make the title. Thank you
+  // <https://stackoverflow.com/a/42755730>!
+  const title = $derived(kind[0].toUpperCase() + kind.substring(1));
+</script>
+
+<div class="callout">
+  <strong class="title">{title}</strong>
+  <div>{@render body()}</div>
+</div>
+
+<style>
+  /* TODO */
+</style>
