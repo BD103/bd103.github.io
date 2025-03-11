76a1a93861451490d7578815e68b0b673157fc2b
BD103 <59022059+BD103@users.noreply.github.com>
2025-03-11 10:31:22 -0400

    feat: add github-flavored markdown formatter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,24 @@
-import { mdsvex } from "mdsvex";
 import adapter from "@sveltejs/adapter-static";
+import gfm from "remark-gfm";
+import { mdsvex } from "mdsvex";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  preprocess: [vitePreprocess(), mdsvex()],
+  preprocess: [
+    vitePreprocess(),
+    mdsvex({
+      remarkPlugins: [gfm],
+      smartypants: {
+        quotes: true,
+        ellipses: true,
+        backticks: false,
+        dashes: true,
+      },
+    }),
+  ],
 
   kit: {
     adapter: adapter({
